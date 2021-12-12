8f37fd7e6022682554c3abef1a78d1e3df3a61a9
dankarger <karger.dan@gmail.com>
2021-12-12 14:52:09 +0200

    2-2 - basic jsx

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import HelloWorld from "./components/2.1-hello_world/HelloWorld.jsx"
+import Basic_of_jsx from "./components/2.2-basics_of_jsx/Basic_of_jsx";
+
 
 const App = ()=> {
 
-    return  <HelloWorld />
+    return(
+
+        <Basic_of_jsx />
+
+    )
 
 
 
---
+++ src/components/2.2-basics_of_jsx/Basic_of_jsx.jsx
@@ -0,0 +1,19 @@
+
+function BasicOfJsx() {
+    const data = ["hello","world"];
+    const number1 = 5;
+    const number2 = 6;
+    const string = 'I Love React'
+
+    return (
+        <div>
+            <p> {data[0]+' ' +data[1]}</p>
+            <p>   {number1+ '+' +number2 } =  {number1 + number2}</p>
+            <p>the String's length is {string.length}</p>
+        </div>
+    )
+}
+
+
+
+export default BasicOfJsx;
