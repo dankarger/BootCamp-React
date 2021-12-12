d134d16864d1b5a4f20b3f177ef48860e7b4b2c7
dankarger <karger.dan@gmail.com>
2021-12-12 13:36:46 +0200

    2-1 hello-world

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+//
+
+// if (module.hot) {
+//     module.hot.accept();
+// }
+
+function getButtonText() {
+    return 'Click Me';
+}
+
+const App = function() {
+    const buttonText = 'Click Me';
+    return (
+
+        <div>
+    Hi there!
+    <button>{getButtonText()}</button>
+
+        </div>
+    )
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App />,
+  document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
